ff01149a7aa78acc3d3ca973a4f62af38ad17ef3
alex3469 <brunet.alex34@gmail.com>
2017-02-09 17:20:10 +0100

    creation de personnage

--- src/AppBundle/Controller/PlayersController.php
+++ src/AppBundle/Controller/PlayersController.php
@@ -8,6 +8,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Joueur;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,9 +23,29 @@ class PlayersController extends Controller {
     
     /**
      * @Route("/players/add", name="addPlayers")
+     * @Method({"POST"})
      * @param Request $r
      */
     public function addPlayers(Request $r) {
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        for($i=1; $i <= 4; $i++){
+            //stockage de la valeur dans la variable email
+            $email = $r->get('j'.strval($i));
+            if ($email != null){
+                //si nouveau joueur
+                $joueur = new Joueur();
+                $joueur->setEmail($email);
+                //mise en session du joueur      
+                $entityManager->persist($joueur);
+                //on ajoute a la variable de session le joueur qui vient d'etre enregistrer
+                $r->getSession()->set('j', strval($i), $joueur);
+            }
+            $entityManager->flush();
+        }
+        
+        
+        
         return $this->redirectToRoute('createPerso');
         
     }
